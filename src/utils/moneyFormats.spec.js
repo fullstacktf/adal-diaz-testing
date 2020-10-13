1a9e53c07082813075785b69e36b9e9e4d3f1021
Adal Díaz Fariña <diazfarina.adal@gmail.com>
2020-10-13 17:20:05 +0100

    ✔ Create moneyFormats.js

---
+++ src/utils/moneyFormats.spec.js
@@ -0,0 +1,28 @@
+const moneyFormat = require("./moneyFormats");
+
+assertEquals(moneyFormat(19.99), "$19.99");
+
+assertEquals(moneyFormat(250), "$250.00");
+
+assertEquals(moneyFormat(9500.95), "$9500.95");
+
+function assertEquals(result, expectedResult) {
+    if (result !== expectedResult) {
+        throw new Error(
+            `This test is failing: Expected "${expectedResult}", got "${result}"`,
+        );
+    }
+}
+console.log("All OK");
+
+/*
+const value1 = moneyFormat(19.99)
+console.log(value1)
+
+const value2 = moneyFormat(250)
+console.log(value2)
+
+const value3 = moneyFormat(9500.95)
+console.log(value3)
+
+*/
